4e48e6cbae0207033c2ce48cb140039dbd734c36
nageshkumar74 <82663413+nageshkumar74@users.noreply.github.com>
2025-06-18 19:56:16 +0530

    Today Assignemnt Given by Kajal Mam
    1 How do you reverse a string in Java
    2 How do you swap two numbers without using a third variable in Java
    3 Write a Java program to check if a vowel is present in a string
    4 Write a Java program to check if the given number is a prime

---
+++ Swap.java
@@ -0,0 +1,14 @@
+
+public class Swap {
+ 
+    public static void main (String [] args){
+        int a=50,b=10;
+        a=a+b; // 60
+        b=a-b; // 50
+        a=a-b;
+        System.out.println("After swapping a="+a +"b="+b);
+
+        
+
+    }
+}
